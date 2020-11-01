f56aebf90c57b04cdab7ab08999542d906c23c8f
Vivelin <1766841+Vivelin@users.noreply.github.com>
2020-11-01 13:58:40 +0100

    Added true file size optimization

--- src/Vivelin.Toolkit/ImageOptimizer.cs
+++ src/Vivelin.Toolkit/ImageOptimizer.cs
@@ -5,7 +5,9 @@ using System.Threading;
 using System.Threading.Tasks;
 
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
 using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Formats.Png;
 using SixLabors.ImageSharp.Processing;
 
 namespace Vivelin.Toolkit
@@ -31,7 +33,7 @@ namespace Vivelin.Toolkit
         {
             try
             {
-                var format = await Image.DetectFormatAsync(_configuration, source);
+                var originalFormat = await Image.DetectFormatAsync(_configuration, source);
 
                 using var image = await Image.LoadAsync(_configuration, source, cancellationToken);
                 if (image.Width > options.TargetWidth || image.Height > options.TargetHeight)
@@ -44,20 +46,72 @@ namespace Vivelin.Toolkit
                 }
 
                 var buffer = new MemoryStream();
-                if (format is JpegFormat)
+                IImageEncoder encoder = null;
+                do
                 {
-                    var encoder = new JpegEncoder { Quality = options.Quality };
-                    await image.SaveAsJpegAsync(buffer, cancellationToken);
-                    return new OptimizedImage(buffer, ImageFormat.Jpeg);
+                    buffer.SetLength(0); // Reset the buffer
+                    encoder = SelectEncoder(encoder, originalFormat, options);
+                    await image.SaveAsync(buffer, encoder, cancellationToken);
                 }
+                while (buffer.Length > options.TargetSize.Bytes);
 
-                await image.SaveAsPngAsync(buffer, cancellationToken);
-                return new OptimizedImage(buffer, ImageFormat.Png);
+                return new OptimizedImage(buffer, GetImageFormat(encoder));
             }
             catch (UnknownImageFormatException ex)
             {
                 throw new ArgumentException("The source does not contain a valid image or the image is of an unsupported format.", ex);
             }
+        }
+
+        /// <summary>
+        /// Selects an encoder that should result in a lower image file size.
+        /// </summary>
+        /// <param name="previousEncoder">The previously used encoder, or <c>null</c>.</param>
+        /// <param name="originalFormat">The format of the original image.</param>
+        /// <param name="options">The options.</param>
+        /// <returns>A new or changed encoder.</returns>
+        private IImageEncoder SelectEncoder(IImageEncoder previousEncoder, IImageFormat originalFormat, ImageOptimizerOptions options)
+        {
+            if (previousEncoder is null)
+            {
+                return originalFormat switch
+                {
+                    JpegFormat _ => new JpegEncoder
+                    {
+                        Quality = options.InitialJpegQuality
+                    },
+                    PngFormat _ => new PngEncoder(),
+                    _ => new PngEncoder()
+                };
+            }
+
+            if (previousEncoder is PngEncoder)
+            {
+                return new JpegEncoder
+                {
+                    Quality = options.InitialJpegQuality
+                };
+            }
+
+            if (previousEncoder is JpegEncoder jpegEncoder)
+            {
+                jpegEncoder.Quality -= 5;
+                if (jpegEncoder.Quality < options.MinimumJpegQuality)
+                    throw new InvalidOperationException("Unable to optimize image without reducing quality below minimum.");
+                return jpegEncoder;
+            }
+
+            throw new NotSupportedException("Unable to select encoder settings based on previous encoder.");
+        }
+
+        private ImageFormat GetImageFormat(IImageEncoder encoder)
+        {
+            return encoder switch
+            {
+                JpegEncoder _ => ImageFormat.Jpeg,
+                PngEncoder _ => ImageFormat.Png,
+                _ => ImageFormat.None
+            };
         }
     }
 }
--- src/Vivelin.Toolkit/ImageOptimizerOptions.cs
+++ src/Vivelin.Toolkit/ImageOptimizerOptions.cs
@@ -15,10 +15,10 @@ namespace Vivelin.Toolkit
         public FileSize TargetSize { get; set; }
             = FileSize.MB(8);
 
-        public int Quality { get; set; }
-            = 90;
+        public int InitialJpegQuality { get; set; }
+            = 95;
 
-        public int MinimumQuality { get; set; }
+        public int MinimumJpegQuality { get; set; }
             = 50;
     }
 }
